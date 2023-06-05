d8c7062a4ddbf9a408c021d7cc1547321d061b16
Mohamed Mansur <M.Kinawy27@gmail.com>
2023-06-06 01:16:46 +0300

    JS_Task_last_version

--- JS_integrated_App_2/App-JS.js
+++ JS_integrated_App_2/App-JS.js
@@ -1,32 +1,24 @@
 const addTask = document.getElementById('add');
 const listOfTaskes = document.getElementById('insert-Task');
-const arr = ["low", "Low", "intermidiate", "Intermidiate", "high", "High"];
 let taskArray = [];
 let noDublicate = [];
 let tasksArray = [];
 let taskIndex = 0;
 
-function validation(x, y) {
-    x === "" || arr.includes(y) === false ||  noDublicate.includes(x) === true ? alert('you entered a wrong ethier task name or priority or repeated task name') : x, y;
+function validation(x) {
+    x === "" || tasksArray.includes(x) === true ? alert('you entered a wrong or repeated task name') : x;
 }
 
 addTask.addEventListener('click', () => {
     let taskName = document.getElementById('task').value;
-    let priorityLevel = document.getElementById('priority').value;
-    validation(taskName, priorityLevel)
-    if (taskName != "" && arr.includes(priorityLevel) != false && noDublicate.includes(taskName) === false) {
+    let selectedPriority = document.getElementById('priority').value;
+    validation(taskName);
+    if (taskName != "" && tasksArray.includes(taskName) === false) {
         //document.getElementById('task').value = '';
-        //document.getElementById('priority').value = '';
-        // sorting table rows
-        noDublicate.push(taskName);
-        taskArray.push(taskName);
-        taskArray.push(priorityLevel);
-        tasksArray.push(taskArray);
+        // sorting table rows with no dublicate task name
+        tasksArray.push(taskName);
         tasksArray.sort();
-        taskIndex = tasksArray.indexOf(taskArray);
-        taskArray = [];
-        console.log(noDublicate);
-        console.log(tasksArray);
+        taskIndex = tasksArray.indexOf(taskName);
         // Adding new rows and columns
         const row = listOfTaskes.insertRow(taskIndex);
         const cell1 = row.insertCell(0);
@@ -35,10 +27,10 @@ addTask.addEventListener('click', () => {
         const cell4 = row.insertCell(3);
         row.setAttribute("id", "new-row");
         // Assigning cells value
-        let x = document.getElementById('insert-Task').rows.length;
-        cell1.innerHTML = x;
+        let count = listOfTaskes.rows.length;
+        cell1.innerHTML = count;
         cell2.innerHTML = taskName;
-        cell3.innerHTML = priorityLevel;
+        cell3.innerHTML = selectedPriority;
         // Defining Button
         const removeButton = document.createElement('Button');
         const editButton = document.createElement('Button');
@@ -54,22 +46,29 @@ addTask.addEventListener('click', () => {
         // Take actions onclick for each new button
         removeButton.addEventListener('click', () => {
             row.style.display = 'none';
+            tasksArray.splice(tasksArray.indexOf(cell2.innerHTML), 1);
         });
         editButton.addEventListener('click', () => {
             editButton.style.display = 'none';
+            addTask.disabled = true;
+            const targetIndex = tasksArray.indexOf(cell2.innerHTML);
+            tasksArray.splice(tasksArray.indexOf(cell2.innerHTML), 1);
             // Creating new inputs when edit button takes action
-            const newInput1 = document.createElement('input');
-            const newInput2 = document.createElement('input');
-            newInput1.setAttribute('placeholder', 'please edit your task name');
-            newInput2.setAttribute('placeholder', 'please edit your priority level');
-            newInput1.setAttribute('id', 'new-input-1');
-            newInput2.setAttribute('id', 'new-input-2');
-            newInput1.setAttribute('value', cell2.innerHTML);
-            newInput2.setAttribute('value', cell3.innerHTML);
+            const newTaskName = document.createElement('input');
+            const priorityLevels = document.createElement('select');
+            newTaskName.setAttribute('placeholder', 'please edit your task name');
+            newTaskName.setAttribute('id', 'new-input-1');
+            newTaskName.setAttribute('value', cell2.innerHTML);
+            priorityLevels.setAttribute('id', 'priority');
+            priorityLevels.insertAdjacentHTML("beforeend", `
+                <option>low</option>
+                <option>intermediate</option>
+                <option>high</option>
+            `);
             cell2.innerHTML = "";
             cell3.innerHTML = "";
-            cell2.appendChild(newInput1);
-            cell3.appendChild(newInput2);
+            cell2.appendChild(newTaskName);
+            cell3.appendChild(priorityLevels);
             // Creating two more buttons save & delete
             const saveButton = document.createElement('Button');
             const deleteButton = document.createElement('Button');
@@ -83,25 +82,22 @@ addTask.addEventListener('click', () => {
             saveButton.setAttribute("id", "save-button");
             deleteButton.setAttribute("id", "delete-button");
             saveButton.addEventListener('click', () => {
-                validation(newInput1.value, newInput2.value);
-                if (newInput1.value != "" && arr.includes(newInput2.value) != false && noDublicate.includes(taskName) === false) {
+                if (newTaskName.value != "" && tasksArray.includes(newTaskName.value) === false) {
                     saveButton.style.display = 'none';
                     deleteButton.style.display = 'none';
                     editButton.style.display = 'inline';
-                    cell2.innerHTML = newInput1.value;
-                    cell3.innerHTML = newInput2.value;
+                    addTask.disabled = false;
+                    cell2.innerHTML = newTaskName.value;
+                    cell3.innerHTML = priorityLevels.value;
+                    tasksArray.push(cell2.innerHTML);
+                    tasksArray.sort();
+                } else {
+                    alert('please enter a correct task name')
                 }
             });
             deleteButton.addEventListener('click', () => {
-                newInput1.value = '';
-                newInput2.value = '';
+                newTaskName.value = '';
             });
         });
     }
 });
-
-
-
-
-
-
